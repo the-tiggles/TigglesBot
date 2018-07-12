dece037ee0951787c49c58ea11ea5d6f443cbe48
Tiggs <mark.palomino@speakcreative.com>
2018-07-12 09:28:59 -0500

    test

--- index.js
+++ index.js
@@ -80,8 +80,10 @@ bot.on("message", async message => {
   if (message.author.bot) return;
   if (message.channel.type === "dm") return message.reply("lemme ask for you")
     .then(sent => console.log('\x1b[42m%s\x1b[0m', `(BotPM)${message.author.username}: ${message.content}`))
-    .then(message.content).send
+    .then(`sent => bot.users.get("333412513385545728").send(${message.content})`);
+    // .then(bot.users.get("333412513385545728")).send(message.content)
     // .catch(console.error);
+
   let prefix = botconfig.prefix;
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
