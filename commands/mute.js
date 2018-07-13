b2136bc7ac963586976e7ff6a3f48aac2efbd7cc
Tiggs <mark.palomino@speakcreative.com>
2018-07-13 07:19:40 -0500

    test

--- commands/mute.js
+++ commands/mute.js
@@ -7,40 +7,44 @@ module.exports.run = async (bot, message, args) => {
 	//!mute @user 1s/m/h/d
 
 
-	// let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-	// if (!tomute) return message.reply("Couldn't find user.");
-	// if(tomute.hasPermission("MANAGE_MESSAGES")) return message.reply("Can't mute them!");
-	// let muterole = message.guild.roles.find(`name`, "muted");
+	let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+	
+	// non-admin trying command
+	if(!message.member.hasPermission("VIEW_AUDIT_LOG")) return message.reply("Try again when you're older, kid");
 
-	// // start of create role
-	// if(!muterole) {
-	// 	try {
-	// 		muterole = await message.guild.createRole({
-	// 			name: "muted",
-	// 			color: "#000000",
-	// 			permissions:[]
-	// 		});
-	// 		message.guild.channels.forEach(async (channel, id) => {
-	// 			await channel.overwritePermissions(muterole, {
-	// 				SEND_MESSAGES: false,
-	// 				ADD_REACTIONS: false
-	// 			});
-	// 		});
-	// 	}catch(e){
-	// 		console.log(e.stack);
-	// 	}
-	// } //end of create role
+	if (!tomute) return message.reply("Couldn't find user.");
+	if(tomute.hasPermission("MANAGE_MESSAGES")) return message.reply("Can't mute them!");
+	let muterole = message.guild.roles.find(`name`, "muted");
 
-	// let mutetime = args[1];
-	// if(!mutetime) return message.reply("You didn't specify a time!");
+	// start of create role
+	if(!muterole) {
+		try {
+			muterole = await message.guild.createRole({
+				name: "muted",
+				color: "#000000",
+				permissions:[]
+			});
+			message.guild.channels.forEach(async (channel, id) => {
+				await channel.overwritePermissions(muterole, {
+					SEND_MESSAGES: false,
+					ADD_REACTIONS: false
+				});
+			});
+		}catch(e){
+			console.log(e.stack);
+		}
+	} //end of create role
 
-	// await(tomute.addRole(muterole.id));
-	// message.reply(`<@${tomute.id}> has been muted for ${ms(ms(mutetime))}`);
+	let mutetime = args[1];
+	if(!mutetime) return message.reply("You didn't specify a time!");
 
-	// setTimeout(function(){
-	// 	tomute.removeRole(muterole.id);
-	// 	message.channel.send(`<@${tomute.id}> has been unmuted!`);
-	// }, ms(mutetime));
+	await(tomute.addRole(muterole.id));
+	message.reply(`<@${tomute.id}> has been muted for ${ms(ms(mutetime))}`);
+
+	setTimeout(function(){
+		tomute.removeRole(muterole.id);
+		message.channel.send(`<@${tomute.id}> has been unmuted!`);
+	}, ms(mutetime));
 
 } //end of module
 
