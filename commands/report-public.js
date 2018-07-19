ab13ac39c09b39cf475c57fbbdfe446c4afa9dfa
Tiggs <mark.palomino@speakcreative.com>
2018-07-19 16:43:47 -0500

    remove console log on report

--- commands/report-public.js
+++ commands/report-public.js
@@ -18,7 +18,6 @@ module.exports.run = async (bot, message, args) => {
  
   message.delete().catch(O_o = {});
   reportschannel.send(reportEmbed);
-  console.log(theirPic);
 }
 
 module.exports.help = {
