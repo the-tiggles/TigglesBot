257abef48b9c0e3e02b1b9e96977909f9fa96b8d
Tiggs <mark.palomino@speakcreative.com>
2018-07-19 18:02:22 -0500

    help start

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,15 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
+    let boticon = bot.user.displayAvatarURL;
+    let helpEmbed = new Discord.RichEmbed()
+        .setDescription("Helpful helps")
+        .setColor("#ffffff")
+        .setThumbnail(boticon)
+        .addField("Field1", "this is the value");
+    return message.channel.send(helpEmbed);
+
+
 
 }
 
