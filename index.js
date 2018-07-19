039d38d92f7a64cdb4d074d8e9725cd9944bf666
Tiggs <mark.palomino@speakcreative.com>
2018-07-19 15:46:19 -0500

    Update

--- index.js
+++ index.js
@@ -105,7 +105,7 @@ bot.on("message", async message => {
   // eyes everywhere
     console.log(`[ ${message.guild} | ${message.channel.name} ] ${message.author.username}: ${message.content}`);
  
- 
+   
 
         // 
         //   - Auto Responses
