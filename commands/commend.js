1b5b6bae7292a865a05eff4233eb4d40f55c30f1
Tiggs <mark.palomino@speakcreative.com>
2018-07-19 16:59:33 -0500

    commending

--- commands/commend.js
+++ commands/commend.js
@@ -1,25 +1,25 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-  let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-  if (!rUser) return message.channel.send("Couldn't find User.");
+  let cUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+  if (!cUser) return message.channel.send("Couldn't find User.");
   let reason = args.join(" ").slice(22);
-  let theirPic = rUser.user.displayAvatarURL;
-  let reportEmbed = new Discord.RichEmbed()
+  let theirPic = cUser.user.displayAvatarURL;
+  let commendEmbed = new Discord.RichEmbed()
     .setAuthor(`${message.author.username}'s Report Summary`, `${message.author.avatarURL}`)
-    .setColor("#e03721")
+    .setColor("#95ff52")
     .setThumbnail(theirPic)
-    .addField("Reported User", `${rUser} with ID: ${rUser.id}`)
+    .addField("Commended User", `${cUser} with ID: ${cUser.id}`)
     .addField("Channel", message.channel)
     .addField("Time", message.createdAt)
     .addField("Reason", reason);
-  let reportschannel = message.guild.channels.find(`name`, "naughty-or-nice");
-  if (!reportschannel) return message.channel.send("Couldn't find reports channel.");
+  let commendchannel = message.guild.channels.find(`name`, "naughty-or-nice");
+  if (!commendchannel) return message.channel.send("Couldn't find reports channel.");
  
   message.delete().catch(O_o = {});
-  reportschannel.send(reportEmbed);
+  commendchannel.send(commendEmbed);
 }
 
 module.exports.help = {
-  name: "report"
+  name: "commend"
 }
