632acba22879f7dc542d3698f4700e5e61f2c21e
Tiggs <mark.palomino@speakcreative.com>
2018-12-25 21:50:44 -0500

    test

---
+++ commands/testreport.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+
+const mongoose = require("mongoose");
+
+mongoose.connect('')
+
+module.exports.run = async (bot, message, args) => {
+
+
+  //,report @bob this is the reason
+  let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+  if (!rUser) return message.channel.send("Couldn't find User.");
+  let reason = args.join(" ").slice(22);
+  let reportEmbed = new Discord.RichEmbed()
+    .setDescription("Reports")
+    .setColor("#e03721")
+    .addField("Reported user", `${rUser} with ID: ${rUser.id}`)
+    .addField("Reported By", `${message.author} with ID: ${message.author.id}`)
+    .addField("Channel", message.channel)
+    .addField("Time", message.createdAt)
+    .addField("Reason", reason);
+  let reportschannel = message.guild.channels.find(`name`, "cookie-jar");
+  if (!reportschannel) return message.channel.send("Couldn't find reports channel.")
+  
+  message.delete().catch(O_o = {});
+  reportschannel.send(reportEmbed);
+  // return;
+}
+
+module.exports.help = {
+  name: "reportp"
+}
